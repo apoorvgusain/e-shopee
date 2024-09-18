cd603c19d0ea6decf9652012fc5efd7a8fea662b
Apoorv Gusain <apoorvgusain123@gmail.com>
2024-09-18 22:44:46 +0530

    Footer Text Change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,15 +15,7 @@ const Footer = () => {
           </a>
           {/* para  */}
           <p className="text-sm text-gray-100 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2024 eshop —
-            <Link
-              to={"/"}
-              className="text-gray-100 ml-1"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              @ebharat
-            </Link>
+            © 2024 eshop
           </p>
 
           {/* media icon  */}
